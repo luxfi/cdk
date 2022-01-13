b5a37ef5fb620cb63b9b91bc461bd8f22f02023e
Ari Lerner <me@ari.io>
2022-01-12 21:36:18 -0500

    WIP

--- lib/monitor-node.ts
+++ lib/monitor-node.ts
@@ -1,9 +1,11 @@
 import { Construct } from "constructs";
 // import { Names } from "cdk8s";
-import * as c from "cdk8s";
+// import * as c from "cdk8s";
 import * as k from "../imports/k8s";
 import { EnvVar } from "../imports/k8s";
 import * as kplus from "cdk8s-plus-22";
+import * as fs from "fs";
+import * as path from "path";
 // import { createServiceAccount } from "./create-service-account";
 
 export interface MonitorNodeProps {
@@ -14,6 +16,14 @@ export interface MonitorNodeProps {
   readonly volumes?: { [key: string]: kplus.Volume };
 }
 
+/// Use the scripts/gen-cert.ts to generate these
+const cert = fs.readFileSync(
+  path.join(__dirname, "..", "conf", "prometheus", "tls.cert")
+);
+const key = fs.readFileSync(
+  path.join(__dirname, "..", "conf", "prometheus", "tls.key")
+);
+
 export class MonitorNode extends Construct {
   constructor(scope: Construct, id: string, props: MonitorNodeProps) {
     super(scope, id, {});
@@ -23,105 +33,268 @@ export class MonitorNode extends Construct {
     // const args = props.args || defaultArgs;
     // const label = { app: Names.toDnsLabel(this) };
     const replicas = props.replicas ?? 1;
-    const env = props.env || {};
-    // const volumes = props.volumes || {};
-    const servicePorts: kplus.ServicePort[] = props.servicePorts || [
-      {
-        port: 3000,
-        name: "web-dashboard",
-        protocol: kplus.Protocol.TCP,
-        targetPort: 3000,
+    // const env = props.env || {};
+
+    // ============= Prometheus
+    new k.KubeClusterRole(this, `prometheus-role`, {
+      metadata: {
+        name: `prometheus-role`,
       },
-      {
-        port: 9090,
-        name: "prometheus-dashboard",
-        protocol: kplus.Protocol.TCP,
-        targetPort: 9090,
+      rules: [
+        {
+          apiGroups: [""],
+          resources: ["nodes", "nodes/proxy", "services", "endpoints", "pods"],
+          verbs: ["get", "list", "watch"],
+        },
+        {
+          apiGroups: ["extensions"],
+          resources: ["ingresses"],
+          verbs: ["get", "list", "watch"],
+        },
+        {
+          nonResourceUrLs: ["/metrics"],
+          verbs: ["get"],
+        },
+      ],
+    });
+    new k.KubeClusterRoleBinding(this, `prometheus`, {
+      metadata: {
+        name: `prometheus`,
       },
-      // {
-      //   name: `node-port`,
-      //   port: 9650,
-      // },
-      // {
-      //   name: `view-port`,
-      //   port: 9090,
-      // },
+      roleRef: {
+        apiGroup: "rbac.authorization.k8s.io",
+        kind: `ClusterRole`,
+        name: `prometheus`,
+      },
+      subjects: [
+        { kind: `ServiceAccount`, name: "default", namespace: "monitoring" },
+      ],
+    });
+
+    const prometheusConfigMap = new kplus.ConfigMap(
+      this,
+      `prometheus-config-map`
+    );
+    prometheusConfigMap.addDirectory(
+      path.join(__dirname, "..", "conf", "prometheus")
+    );
+    const prometheusConfigVolume =
+      kplus.Volume.fromConfigMap(prometheusConfigMap);
+    const prometheusStorageVolume = kplus.Volume.fromEmptyDir(
+      `prometheus-storage-volume`
+    );
+    const prometheusVolumeMounts = [
+      { name: prometheusConfigVolume.name, mountPath: `/etc/prometheus` },
+      { name: prometheusStorageVolume.name, mountPath: `/prometheus/` },
     ];
+
+    new k.KubeDeployment(this, `prometheus-deployment`, {
+      metadata: {
+        namespace: "monitoring",
+        name: "prometheus-deployment",
+        labels: { app: "prometheus-server" },
+      },
+      spec: {
+        replicas,
+        selector: { matchLabels: { app: "prometheus-server" } },
+        template: {
+          metadata: { labels: { app: "prometheus-server" } },
+          spec: {
+            containers: [
+              {
+                image,
+                name: "prometheus",
+                imagePullPolicy: kplus.ImagePullPolicy.NEVER,
+                args: [
+                  "--config.file=/etc/prometheus/prometheus.yml",
+                  "--storage.tsdb.path=/prometheus/",
+                ],
+                ports: [{ containerPort: 9090 }],
+                volumeMounts: prometheusVolumeMounts,
+              },
+            ],
+            volumes: [
+              {
+                name: prometheusConfigVolume.name,
+                configMap: { defaultMode: 420, name: `prometheus-server-conf` },
+              },
+              { name: prometheusStorageVolume.name },
+            ],
+          },
+        },
+      },
+    });
+
+    new k.KubeService(this, `prometheus-service`, {
+      metadata: {
+        name: "prometheus-service",
+        namespace: "monitoring",
+        annotations: {
+          "prometheus.io/scrape": "true",
+          "prometheus.io/port": "9090",
+        },
+      },
+      spec: {
+        selector: { app: "prometheus-server" },
+        ports: [
+          {
+            port: 8080,
+            targetPort: k.IntOrString.fromNumber(9090),
+            nodePort: 30000,
+          },
+        ],
+        type: `NodePort`,
+      },
+    });
+
+    new k.KubeIngress(this, `prometheus-ui`, {
+      metadata: {
+        namespace: "monitoring",
+        name: "prometheus-ui",
+        annotations: {
+          "kubernetes.io/ingress.class": "nginx",
+        },
+      },
+      spec: {
+        rules: [
+          {
+            host: `prometheus.lux`,
+            http: {
+              paths: [
+                {
+                  path: "/dashboard",
+                  pathType: "Prefix",
+                  backend: {
+                    service: {
+                      name: `prometheus-service`,
+                      port: { number: 8080 },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+        tls: [
+          {
+            hosts: [`prometheus.lux`],
+            secretName: `prometheus-secret`,
+          },
+        ],
+      },
+    });
+
+    new k.KubeSecret(this, `prometheus-secret`, {
+      metadata: {
+        name: "prometheus-secret",
+        namespace: "monitoring",
+      },
+      data: {
+        "tls.crt": Buffer.from(cert).toString("base64"),
+        "tls.key": Buffer.from(key).toString("base64"),
+      },
+    });
+
+    /// =============== /Prometheus
+
+    // const volumes = props.volumes || {};
+    // const servicePorts: kplus.ServicePort[] = props.servicePorts || [
+    //   {
+    //     port: 3000,
+    //     name: "web-dashboard",
+    //     protocol: kplus.Protocol.TCP,
+    //     targetPort: 3000,
+    //   },
+    //   {
+    //     port: 9090,
+    //     name: "prometheus-dashboard",
+    //     protocol: kplus.Protocol.TCP,
+    //     targetPort: 9090,
+    //   },
+    // {
+    //   name: `node-port`,
+    //   port: 9650,
+    // },
+    // {
+    //   name: `view-port`,
+    //   port: 9090,
+    // },
+    // ];
 
     // createServiceAccount(this, { name: `monitor` });
 
     // const service =
-    new kplus.Service(this, `monitor-service`, {
-      ports: servicePorts,
-      type: kplus.ServiceType.CLUSTER_IP,
-      metadata: {
-        labels: {
-          run: "monitor-node",
-        },
-      },
-    });
+    // new kplus.Service(this, `monitor-service`, {
+    //   ports: servicePorts,
+    //   type: kplus.ServiceType.CLUSTER_IP,
+    //   metadata: {
+    //     labels: {
+    //       run: "monitor-node",
+    //     },
+    //   },
+    // });
 
-    const deployment = new kplus.Deployment(this, `node-deployment`, {
-      metadata: {
-        labels: {
-          run: "monitor-node",
-        },
-      },
-      replicas,
-      // port: 9090,
-    });
-    // deployment.selectByLabel("run", "monitor-node");
+    // const deployment = new kplus.Deployment(this, `node-deployment`, {
+    //   metadata: {
+    //     labels: {
+    //       run: "monitor-node",
+    //     },
+    //   },
+    //   replicas,
+    //   // port: 9090,
+    // });
+    // // deployment.selectByLabel("run", "monitor-node");
 
-    // const container =
-    deployment.addContainer({
-      image,
-      // Uncomment this for local testing purposes
-      // In order to test local docker images
-      // eval $(minikube docker-env)
-      // then build your docker image
-      imagePullPolicy: kplus.ImagePullPolicy.NEVER,
-      env,
-    });
+    // // const container =
+    // deployment.addContainer({
+    //   image,
+    //   // Uncomment this for local testing purposes
+    //   // In order to test local docker images
+    //   // eval $(minikube docker-env)
+    //   // then build your docker image
+    //   imagePullPolicy: kplus.ImagePullPolicy.NEVER,
+    //   env,
+    // });
 
-    const envs = Object.keys(env).reduce((acc: any[], key: string) => {
-      acc.push({ key, name: env[key] });
-      return acc;
-    }, []);
-    const kubeDeployment = c.ApiObject.of(deployment);
-    kubeDeployment.addJsonPatch(
-      c.JsonPatch.replace("/spec/selector/matchLabels", {
-        run: "monitor-node",
-      })
-    );
-    kubeDeployment.addJsonPatch(
-      c.JsonPatch.add("/spec/template", {
-        metadata: {
-          labels: {
-            "cdk8s.deployment": deployment.name,
-            run: "monitor-node",
-          },
-        },
-        spec: {
-          containers: [
-            {
-              env: envs,
-              image,
-              imagePullPolicy: kplus.ImagePullPolicy.NEVER,
-              name: `main`,
-              resources: {
-                requests: {
-                  cpu: k.Quantity.fromString("50m"),
-                  memory: k.Quantity.fromString("512Mi"),
-                },
-                limits: {
-                  cpu: k.Quantity.fromString("50m"),
-                  memory: k.Quantity.fromString("512Mi"),
-                },
-              },
-            },
-          ],
-        },
-      })
-    );
+    // const envs = Object.keys(env).reduce((acc: any[], key: string) => {
+    //   acc.push({ key, name: env[key] });
+    //   return acc;
+    // }, []);
+    // const kubeDeployment = c.ApiObject.of(deployment);
+    // kubeDeployment.addJsonPatch(
+    //   c.JsonPatch.replace("/spec/selector/matchLabels", {
+    //     run: "monitor-node",
+    //   })
+    // );
+    // kubeDeployment.addJsonPatch(
+    //   c.JsonPatch.add("/spec/template", {
+    //     metadata: {
+    //       labels: {
+    //         "cdk8s.deployment": deployment.name,
+    //         run: "monitor-node",
+    //       },
+    //     },
+    //     spec: {
+    //       containers: [
+    //         {
+    //           env: envs,
+    //           image,
+    //           imagePullPolicy: kplus.ImagePullPolicy.NEVER,
+    //           name: `main`,
+    //           resources: {
+    //             requests: {
+    //               cpu: k.Quantity.fromString("50m"),
+    //               memory: k.Quantity.fromString("512Mi"),
+    //             },
+    //             limits: {
+    //               cpu: k.Quantity.fromString("50m"),
+    //               memory: k.Quantity.fromString("512Mi"),
+    //             },
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   })
+    // );
   }
 }
---
+++ scripts/gen-certs.ts
@@ -0,0 +1,71 @@
+#!/usr/bin/env ts-node
+
+// const path = require("path");
+// const fs = require("fs");
+const selfsigned = require("selfsigned");
+
+const args = require("yargs")
+  .env("AVA")
+  .options({
+    commonName: {
+      alias: "n",
+      default: `lux`,
+    },
+    countryName: {
+      alias: "c",
+      default: "IM",
+    },
+    localityName: {
+      alias: "l",
+      default: "CITY",
+    },
+    stateOrProvinceName: {
+      alias: "s",
+      default: "CITY",
+    },
+    organizationName: {
+      alias: "o",
+      default: "lux",
+    },
+    organizationalUnitName: {
+      default: "C",
+    },
+    emailAddress: {
+      default: "us@lux",
+      help: "email address",
+    },
+    outDir: {
+      default: path.join(__dirname, "..", "conf", "prometheus"),
+      help: "directory to save the certs",
+    },
+  })
+  .help(`Generate self-signed certificates`)
+  .alias("help", "h").argv;
+
+console.log(`Generating certs`);
+const attrs = [
+  {
+    name: "commonName",
+    value: args.commonName,
+  },
+  { name: "countryName", value: args.countryName },
+  { name: "localityName", value: args.localityName },
+  { name: "stateOrProvinceName", value: args.stateOrProvinceName },
+  { name: "organizationName", value: args.organizationName },
+  { name: "organizationalUnitName", value: args.organizationalUnitName },
+  { name: "emailAddress", value: args.emailAddress },
+];
+
+const outDir = path.normalize(args.outDir);
+const pems = selfsigned.generate(attrs, { days: 365 });
+
+console.log(`Generated pems, saving to ${outDir}`);
+
+const keyFile = path.join(outDir, "tls.key");
+const certFile = path.join(outDir, `tls.cert`);
+const privKey = path.join(outDir, "tls.priv");
+fs.writeFileSync(certFile, pems.cert, "utf-8");
+fs.writeFileSync(keyFile, pems.public, "utf-8");
+fs.writeFileSync(privKey, pems.private, "utf-8");
+
+console.log(`Done. Check your path`);
