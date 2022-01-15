00a2380818d2dcea5c95e256e3d4d545efc4cc66
Ari Lerner <me@ari.io>
2022-01-14 20:28:25 -0500

    Adding curl and jq to mon box

--- lib/monitoring/grafana/import-dashboards/job.ts
+++ lib/monitoring/grafana/import-dashboards/job.ts
@@ -44,28 +44,31 @@ export const job = (c: Construct, opts: GrafanaOptions) => {
               workingDir: "/opt/grafana-import-dashboards",
               args: [
                 `
-            for file in *-datasource.json; do
-              if [ -e "$file" ]; then
-              echo "Importing $file" &&
-              curl --silent --fail --show-error \
-                --request POST http://\${GF_ADMIN_USER}:\${GF_ADMIN_PASSWORD}@grafana:3000/api/datasources \
-                --header "Content-Type: application/json" \
-                --data-binary "@$file";
-                echo "";
+                ls -la && pwd &&
+                        for file in *-datasource.json ; do
+              if [ -e "$file" ] ; then
+                echo "importing $file" &&
+                curl --silent --fail --show-error \
+                  --request POST http://\${GF_ADMIN_USER}:\${GF_ADMIN_PASSWORD}@grafana:3000/api/datasources \
+                  --header "Content-Type: application/json" \
+                  --data-binary "@$file" ;
+                echo "" ;
               fi
-            done;
+            done ;
             for file in *-dashboard.json ; do
-              if [ -e "$file" ]; then
-                echo "Importing $file" &&
+              if [ -e "$file" ] ; then
+                echo "importing $file" &&
                 ( echo '{"dashboard":'; \
                   cat "$file"; \
-                  echo ',"overwrite":true,"inputs":[{"name":"DS_PROMETHEUS",type:"datasource","pluginId":"prometheus","value":"prometheus"}]}' ) \
+                  echo ',"overwrite":true,"inputs":[{"name":"DS_PROMETHEUS","type":"datasource","pluginId":"prometheus","value":"prometheus"}]}' ) \
                 | jq -c '.' \
                 | curl --silent --fail --show-error \
                   --request POST http://\${GF_ADMIN_USER}:\${GF_ADMIN_PASSWORD}@grafana:3000/api/dashboards/import \
                   --header "Content-Type: application/json" \
-                  --data-binary "@$file";
-                    echo "";
+                  --data-binary "@-" ;
+                echo "" ;
+              fi
+            done
             `,
               ],
               env: [
