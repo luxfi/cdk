ce32abb55cd6d6f8b06c7b9e546afdae9c7b83b2
Ari Lerner <me@ari.io>
2022-01-20 13:54:09 -0500

    Added init.sh script

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,5 +5,4 @@ eval $(minikube docker-env);
 npm run init
 helm repo add coredns https://coredns.github.io/helm
 helm --namespace=kube-system install coredns coredns/coredns \
-  --set prometheus.service.annotations=true \
   --set prometheus.service.enabled=true
