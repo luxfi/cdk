1defbf35b528f58b8bacb855ac602d5170e2c6db
Mr. KF <mr.kf1994@gmail.com>
2022-01-21 12:44:50 +0500

    test: added test for block chain health

---
+++ src/monitor/test/blockchain/health.test.ts
@@ -0,0 +1,20 @@
+import {health} from "../../src/lib/ava";
+
+test('address create cli command', async () => {
+    const args = {
+        _: ['blockchain', 'list'],
+        namespace: 'default',
+        networkId: 4200,
+        host: 'localhost',
+        port: 9650,
+        protocol: 'http',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDIxMDI2NjYsImp0aSI6ImtlSzM3aW5SaW5VRldFTG03V3gxeHhRLTQwdz0iLCJlbmRwb2ludHMiOlsiKiJdfQ.dYwxkpGkypG7BPQfCQcMgg-3cGWu3OXzdd8qV8VUu0E',
+        t: 'blockchains',
+        '$0': 'build\\cli.js'
+    }
+
+    const resp = await health.health(args);
+    const exitStatus = JSON.parse(resp.exitStatus);
+    expect(exitStatus.status).toEqual("Success");
+    expect(resp.data.checks).toEqual(expect.any(Object));
+})
