672f5d47ff35e2e5277b0258bb7d5e7373c53395
Mr. KF <mr.kf1994@gmail.com>
2022-01-26 11:55:47 +0500

    feat: fixed blockchain list command to show proper result

--- src/monitor/src/cmd/blockchain/list.tsx
+++ src/monitor/src/cmd/blockchain/list.tsx
@@ -5,6 +5,7 @@ import { platform } from "../../lib/ava";
 import chalk from "chalk";
 import clear from "clear";
 import figlet from "figlet";
+import Table from "cli-table";
 
 export const command = "list [args]";
 
@@ -12,8 +13,8 @@ export const desc = "List all blockchains";
 
 export const builder = (yargs: Argv) =>
   yargs.options({
-    t: {
-      alias: "type",
+    type: {
+      alias: "t",
       describe: "type to fetch",
       choices: ["blockchains", "subnets"],
       default: "blockchains",
@@ -21,13 +22,43 @@ export const builder = (yargs: Argv) =>
   });
 
 const handleGetBlockchains = async (args: ArgShape) => {
-  const resp = await platform.getBlockchains();
-  console.log(resp);
+  const {data} = await platform.getBlockchains(args);
+  if (data.blockchains) {
+    clear();
+    console.log(
+        chalk.yellow(figlet.textSync("Blockchains", { horizontalLayout: "full" }))
+    );
+    const listTable = new Table({
+      head: ["ID", "Name", "Subnet ID", "VM ID"],
+    });
+    data.blockchains.forEach((blockchain: any) => {
+      listTable.push([blockchain.id, blockchain.name, blockchain.subnetID, blockchain.vmID]);
+    })
+    console.log(listTable.toString());
+  } else {
+    console.log(`${chalk.red('Error getting blockchain list!')}`);
+    console.error('Reason:', data.error.message);
+  }
 };
 
 const handleGetSubnets = async (args: ArgShape) => {
-  const resp = await platform.getSubnets();
-  console.log(resp);
+  const {data} = await platform.getSubnets(args);
+  if (data.subnets) {
+    clear();
+    console.log(
+        chalk.yellow(figlet.textSync("Subnets", { horizontalLayout: "full" }))
+    );
+    const listTable = new Table({
+      head: ["ID", "Control Keys", "Threshold"],
+    });
+    data.subnets.forEach((subnet: any) => {
+      listTable.push([subnet.id, subnet.controlKeys, subnet.threshold]);
+    })
+    console.log(listTable.toString());
+  } else {
+    console.log(`${chalk.red('Error getting subnets list!')}`);
+    console.error('Reason:', data.error.message);
+  }
 };
 
 export async function handler(args: ArgShape) {
