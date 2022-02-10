b5259ca742b8c6ddbac2cd64925a403e19592108
Mr. KF <mr.kf1994@gmail.com>
2022-02-10 12:42:32 +0500

    feat: added all info routes

--- rpc/src/api/routes/v1/info.ts
+++ rpc/src/api/routes/v1/info.ts
@@ -6,5 +6,13 @@ import {assignMethod} from "../../../helpers/middlewares";
 const InfoRouter: express.Router = express.Router();
 
 InfoRouter.get("/blockchain-id/:alias", assignMethod('info', 'getBlockchainID'), executeMethodCtrl);
+InfoRouter.get("/network-id/", assignMethod('info', 'getNetworkID'), executeMethodCtrl);
+InfoRouter.get("/network-name/", assignMethod('info', 'getNetworkName'), executeMethodCtrl);
+InfoRouter.get("/node-id/", assignMethod('info', 'getNodeID'), executeMethodCtrl);
+InfoRouter.get("/node-ip/", assignMethod('info', 'getNodeIP'), executeMethodCtrl);
+InfoRouter.get("/node-version/", assignMethod('info', 'getNodeVersion'), executeMethodCtrl);
+InfoRouter.get("/trx-fees/", assignMethod('info', 'getTxFee'), executeMethodCtrl);
+InfoRouter.get("/is-bootstrapped/:chain", assignMethod('info', 'isBootstrapped'), executeMethodCtrl);
+InfoRouter.get("/uptime/", assignMethod('info', 'uptime'), executeMethodCtrl);
 
 export default InfoRouter;
